d7071a4ff594df242bc3c625f3e01c2e2c629e07
Flavia Romito <flavia.romito@alcampus.it>
2025-12-09 16:01:47 +0100

    Scheletro development

---
+++ developement.py
@@ -0,0 +1,13 @@
+class KNNClassifier:
+
+    def __init__(self, k: int):
+        # Inizzializzo
+        self.k = k
+        self.x_training = None
+        self.y_training = None
+
+    def training_set(self, x_training, y_training):
+
+    def distanza_euclidea(self, a, b) -> float:
+
+    def classificazione(self, x_test) -> int|float:
