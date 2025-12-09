cfed3a90a943fd8bd1d2b9e23b34508a2919b665
Gabriele Di Raimondo <g.diraimondo21@gmail.com>
2025-12-09 15:09:56 +0100

    Primo caso di test

--- Test_unit.py
+++ Test_unit.py
@@ -1,8 +1,17 @@
 import unittest
+from Prepocessing import *
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+class TestDataPreprocessing(unittest.TestCase):
+    def test_path_not_found_exception_raised(self):
+        """
+        Verifica che se viene fornito un path errato viene alzata la giusta eccezione
+        """
+        non_existent_path = "path/che/non/esiste/Prova1.csv"
+        p = DataCsv(opener, non_existent_path)
+        with self.assertRaises(FileNotFoundError):
+            p.load()
 
 
+if __name__ == "__main__":
+    unittest.main()
