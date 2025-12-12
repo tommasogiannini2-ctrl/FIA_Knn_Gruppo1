391447baabdf9d6fb5e968be6d919948ee7141d4
Flavia Romito <flavia.romito@alcampus.it>
2025-12-12 11:53:17 +0100

    Aggiunto parser

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 from Prepocessing import *
 from developement import *
 from validation_evaluation_strategies import *
+import argparse
 
-filename = './dati/version_1.csv'
+parser = argparse.ArgumentParser(description='Elabora un dataframe secondo il metodo KKN e calcola le metriche più comuni.')
+
+# definisce l'argomento per il file di ingresso e di uscita con un valore di default
+parser.add_argument('-i', '--input', type=str, default='dati/version_1.csv', help='Inserire percorso del file di ingresso (Default: dati/version_1.csv)')
+parser.add_argument('-o', '--output', type=str, default='risultati.xlsx',help='Inserire percorso del file JSON di uscita (Default: risultati.xlsx)')
+parser.add_argument('-v', '--validation', type=str, default=None, required=True, choices=['RS','KF'], help='Scegliere il metodo di validazione da eseguire (Inserire RS per eseguire il Random Subsampling o KF per eseguire il K-Fold Cross Validation)')
+parser.add_argument('-p', '--percentuale_holdout', type=float, default=0.8, help="Scegliere percentuale per l'holdout (Default: 0.8)")
+parser.add_argument('-K', '--K_prove', type=int, default=5, help='Scegliere il numero di esperimenti da eseguire per il Randoma Subsampling o per il K-Fold Cross Validation (Default=5')
+pars = parser.parse_args()
+
+pars_out = pars.output
+validation_type = pars.validation
+p_Holdout = pars.percentuale_holdout
+n_prove = pars.K_prove
+
+filename = pars.input
 opener = scegli_opener(filename)
 
 dati = Data(opener,filename)
@@ -12,10 +28,8 @@ tupla = dati.load()
 data_unico = unificaDF(tupla[0],tupla[1])
 #print(data_unico)
 # Parametri per la divisione
-n_prove = 5
 data = ValidationStrategy(data_unico)
 p_RandomSubsampling = 0.8
-p_Holdout = 0.8
 
 # Divisione con holdout
 lista_holdout = data.RandomSubsampling(1,p_Holdout)
